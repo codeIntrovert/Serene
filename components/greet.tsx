6c6da99a1f0f42586c09704155e3cf65813f0cf7
Hasan <9131411681hasan@gmail.com>
2024-09-27 21:38:45 +0530

    added greet component

--- components/greet.tsx
+++ components/greet.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 export default function Greet({ usernameProp }: HeaderProps) {
   return (
     <View style={tw`top-5 flex-row w-full`}>
-      <Text style={tw`font-bold text-white text-2xl`}>Hi {usernameProp}</Text>
+      <Text style={tw`font-bold text-white text-2xl`}>Hi {usernameProp} !</Text>
       <Image
         source={{ uri: "https://reactjs.org/logo-og.png" }}
         style={tw`border-2 border-slate-800 absolute right-0 bg-green-400 w-9 h-9 rounded-full`}
