4cba22180e0a545f9a3b8f43a53e47f28ad18c15
Hasan <9131411681hasan@gmail.com>
2024-09-27 21:30:21 +0530

    [mod] therapist inteface added

--- app/therapist.tsx
+++ app/therapist.tsx
@@ -1,22 +1,36 @@
+import React from "react";
 import { Text, View, Image, ScrollView } from "react-native";
 import Nav from "@/components/nav";
 import tw from "twrnc";
 import TherapyCard from "@/components/therapyCard";
+import therapistsData from "../data/therapist.json";
+
+// Define the type for the therapist data
+interface Therapist {
+  id: number;
+  name: string;
+  degree: string;
+  image: string;
+  rating: number;
+}
+
 export default function Therapist() {
   return (
-    <View style={tw``}>
+    <View style={tw`bg-slate-900 h-full`}>
       <Nav />
-      <>
-        <View style={tw`bg-slate-900 h-8 px-6`}></View>
-        <View style={tw`bg-slate-900 h-full px-6`}>
-          <Text style={tw`my-6 font-bold text-white text-2xl `}>
-            Help is always near
-          </Text>
-          <ScrollView>
-            <TherapyCard />
-          </ScrollView>
-        </View>
-      </>
+      <View style={tw`bg-slate-900 h-full pt-6 px-6`}>
+        <Text style={tw`my-6 font-bold text-white text-2xl`}>
+          Help is always near
+        </Text>
+        <ScrollView>
+          {therapistsData.map((therapist: Therapist) => (
+            <View key={therapist.id} style={tw`mb-4`}>
+              <TherapyCard therapist={therapist} />
+            </View>
+          ))}
+          <View style={tw`h-20 w-full`}></View>
+        </ScrollView>
+      </View>
     </View>
   );
 }
