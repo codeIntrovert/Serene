eefcaed25f022f8ce414b3068146baca501486a1
Hasan <46672438+codeIntrovert@users.noreply.github.com>
2024-09-29 22:37:11 +0530

    [FIX] Fixed API security lapse

--- components/response.tsx
+++ components/response.tsx
@@ -4,7 +4,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import Markdown from "react-native-markdown-display";
 
 const date = new Date();
-const API_KEY = "AIzaSyBKKLDPStfxCA1LBgZCCDvO6OFwdpupXcE"; // Ensure this API key is correct
+const API_KEY = "YOUR GEMINI API KE";
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 export default function Response(props: any) {
